a1b2a647fcfd45146b567d681e66e44b0a8fe124
MinmusLin <minmuslin@outlook.com>
2024-09-17 21:01:35 +0800

    +

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,17 @@
 <template>
-  <p>HomePage</p>
+  <div class='text-area'>
+    <h1>一级标题</h1>
+    <h2>二级标题</h2>
+    <h3>三级标题</h3>
+    <h4>四级标题</h4>
+    <h5>五级标题</h5>
+    <p>正文</p>
+  </div>
 </template>
 
 <script setup lang='ts'>
 </script>
 
 <style scoped>
+@import '@/assets/styles/text.css';
 </style>
