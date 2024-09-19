f4a530993fcac0e0a09f638a978a49d5de26f123
MinmusLin <minmuslin@outlook.com>
2024-09-19 10:12:32 +0800

    Update FooterNavbar.vue

--- src/components/FooterNavbar.vue
+++ src/components/FooterNavbar.vue
@@ -2,14 +2,14 @@
   <div class='footer-navbar'>
     <img src='/Logos/TongjiUniversityLogo.png'
          alt='TongjiUniversityLogo'
-         style='top: 50px; left: 102px; cursor: pointer'
+         style='top: 50px; left: 100px; cursor: pointer'
          @click="goToLink('https://en.tongji.edu.cn')">
     <img src='/Logos/TongjiSoftwareLogo.png'
          alt='TongjiSoftwareLogo'
-         style='top: 90px; left: 306px'>
+         style='top: 90px; left: 304px'>
     <img src='/Logos/LightLogo.png'
          alt='LightLogo'
-         style='top: 93px; left: 547px; cursor: pointer'
+         style='top: 93px; left: 545px; cursor: pointer'
          @click="goToLink('http://47.91.92.143')">
     <div class='notes'>
       <p>
@@ -27,16 +27,16 @@
     <a class='email' href='mailto:tongji_china2019@163.com'>tongji_china2019@163.com</a>
     <img src='/DesignMaterials/Email.png'
          alt='Email'
-         style='top: 225px; right: 442px'>
+         style='top: 225px; right: 439px'>
     <img src='/DesignMaterials/TikTok.png'
          alt='TikTok'
-         style='top: 298px; right: 450px'>
+         style='top: 298px; right: 447px'>
     <img src='/DesignMaterials/WeChat.png'
          alt='WeChat'
-         style='top: 296px; right: 324px'>
+         style='top: 296px; right: 321px'>
     <img src='/DesignMaterials/Bilibili.png'
          alt='Bilibili'
-         style='top: 302px; right: 103px'>
+         style='top: 302px; right: 100px'>
   </div>
 </template>
 
@@ -54,15 +54,6 @@ function goToLink(link: string) {
   -webkit-user-select: none;
 }
 
-.notes p {
-  color: white;
-  font-family: 'Futura Bk BT Book', serif;
-  font-weight: 500;
-  line-height: 34px;
-  font-size: 20px;
-  white-space: nowrap;
-}
-
 .contact-us {
   position: absolute;
   color: white;
@@ -70,7 +61,7 @@ function goToLink(link: string) {
   line-height: 34px;
   font-size: 32px;
   font-weight: 700;
-  right: 333px;
+  right: 330px;
   top: 135px;
   white-space: nowrap;
 }
@@ -85,15 +76,24 @@ img {
   font-family: 'Futura Bk BT Book', serif;
   font-size: 20px;
   font-weight: 500;
-  right: 170px;
+  right: 167px;
   top: 225px;
   text-decoration: none;
 }
 
 .notes {
   position: absolute;
-  top: 284px;
-  left: 108px;
+  bottom: 78px;
+  left: 99px;
+}
+
+.notes p {
+  color: white;
+  font-family: 'Futura Bk BT Book', serif;
+  font-weight: 500;
+  line-height: 34px;
+  font-size: 18px;
+  white-space: nowrap;
 }
 
 a {
