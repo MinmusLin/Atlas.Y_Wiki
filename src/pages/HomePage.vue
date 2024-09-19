511850d38f5e90b04c2e0871eb09afdfc4348ae4
MinmusLin <minmuslin@outlook.com>
2024-09-19 14:00:49 +0800

    更新

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -9,105 +9,32 @@
       <a :class="{ active: activeAnchor == '#Title1' }" @click.prevent="scrollToSection('Title1')">Title1</a>
       <a :class="{ active: activeAnchor == '#Title2' }" @click.prevent="scrollToSection('Title2')">Title2</a>
       <a :class="{ active: activeAnchor == '#Title3' }" @click.prevent="scrollToSection('Title3')">Title3</a>
-      <a :class="{ active: activeAnchor == '#Title4' }" @click.prevent="scrollToSection('Title4')">Title4</a>
     </nav>
     <div class='text-area' :style="{ marginTop: (-navHeight-22) + 'px' }">
       <h1 id='Title1'>Title1</h1>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
+      <p>Text</p>
+      <h2>Title1.1</h2>
+      <p>Text</p>
+      <h3>Title1.1.1</h3>
+      <p>Text</p>
+      <h4>Title1.1.1.1</h4>
+      <p>Text</p>
       <h1 id='Title2'>Title2</h1>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <h1 id='Title3'>Title3</h1>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <h1 id='Title4'>Title4</h1>
-      <h2>Title2</h2>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <h3>Title3</h3>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <h4>Title4</h4>
-      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.
-        By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.
-        By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p class='bold'>By visiting the WIKI, you can gain insight into the entire development process, explore our
-        technical details and the thinking behind our decisions, and access a wealth of background information and
-        resource support.
-        By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.
-        By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p class='strikethrough'>By visiting the WIKI, you can gain insight into the entire development process, explore
-        our technical details and the thinking behind our decisions, and access a wealth of background information and
-        resource support.
-        By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.
-        By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p class='italic'>By visiting the WIKI, you can gain insight into the entire development process, explore our
-        technical details and the thinking behind our decisions, and access a wealth of background information and
-        resource support.
-        By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.
-        By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
-      <p class='underline'>By visiting the WIKI, you can gain insight into the entire development process, explore our
-        technical details and the thinking behind our decisions, and access a wealth of background information and
-        resource support.
-        By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.
-        By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
-        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
+      <p class='bold'>bold text</p>
+      <p class='strikethrough'>strikethrough text</p>
+      <p class='italic'>italic text</p>
+      <p class='underline'>underline text</p>
+      <p>Unordered List:</p>
       <ul>
-        <li>By visiting the WIKI, you can gain insight into the entire development process,</li>
-        <li>explore our technical details and the thinking behind our decisions,</li>
-        <li>and access a wealth of background information and resource support.</li>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
       </ul>
+      <p>Ordered List:</p>
       <ol>
-        <li>By visiting the WIKI, you can gain insight into the entire development process,</li>
-        <li>explore our technical details and the thinking behind our decisions,</li>
-        <li>and access a wealth of background information and resource support.</li>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
       </ol>
       <p>
         C<sub>8</sub>H<sub>10</sub>N<sub>4</sub>O<sub>2</sub>
@@ -115,35 +42,43 @@
       <p>
         C<sup>8</sup>H<sup>10</sup>N<sup>4</sup>O<sup>2</sup>
       </p>
+      <h1 id='Title3'>Title3</h1>
+      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
+        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
       <img src='https://th.bing.com/th/id/OIP.tmvIu57hTRpXhPRWkBDDEQHaEK?rs=1&pid=ImgDetMain' alt='ExampleImage'
-           width='400'/>
-      <h5>Example Image</h5>
+           width='600'/>
+      <h5>Figure 1.1 Example</h5>
+      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
+        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
+      <h5>Table 1.1 Example</h5>
       <table>
         <thead>
         <tr>
-          <th>Header 1</th>
-          <th>Header 2</th>
-          <th>Header 3</th>
+          <th>Column 1</th>
+          <th>Column 2</th>
+          <th>Column 3</th>
         </tr>
         </thead>
         <tbody>
         <tr>
-          <td>Row 1, Cell 1</td>
-          <td>Row 1, Cell 2</td>
-          <td>Row 1, Cell 3</td>
+          <td>1-1</td>
+          <td>1-2</td>
+          <td>1-3</td>
         </tr>
         <tr>
-          <td>Row 2, Cell 1</td>
-          <td>Row 2, Cell 2</td>
-          <td>Row 2, Cell 3</td>
+          <td>2-1</td>
+          <td>2-2</td>
+          <td>2-3</td>
         </tr>
         <tr>
-          <td>Row 3, Cell 1</td>
-          <td>Row 3, Cell 2</td>
-          <td>Row 3, Cell 3</td>
+          <td>3-1</td>
+          <td>3-2</td>
+          <td>3-3</td>
         </tr>
         </tbody>
       </table>
+      <p>By visiting the WIKI, you can gain insight into the entire development process, explore our technical details
+        and the thinking behind our decisions, and access a wealth of background information and resource support.</p>
     </div>
   </div>
 </template>
