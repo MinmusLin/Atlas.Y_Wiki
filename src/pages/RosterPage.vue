dc72a89f29c009563d5d4c81cde0339a898a7bde
MinmusLin <minmuslin@outlook.com>
2024-09-28 21:59:00 +0800

    Update RosterPage.vue

--- src/pages/RosterPage.vue
+++ src/pages/RosterPage.vue
@@ -12,11 +12,11 @@
                   name='Shurui Ning'
                   role='Algorithm Development'
                   bio="Hi, I'm Shurui Ning, a bioinformatics student. I'm responsible for team coordination and the development of biological algorithms. I'm thrilled to have met like-minded friends through iGEM, and I'm excited to use bioinformatics methods to contribute to the advancement of synthetic biology."/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/jinlingyuan.webp'
                   name='Jinling Yuan'
                   role='Algorithm Development'
                   bio="Hi! I'm Jinling Yuan, majoring in bioinformatic. I have a profound appreciation for nature and life, possess an insatiable thirst for knowledge and vitality, enjoy both sports and the humanities. I am delighted to connect with you in the iGEM community."/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/jishenlin.webp'
                   name='Jishen Lin'
                   role='Software Development / Wiki coding'
                   bio="Hi! I'm Jishen Lin, majoring in software engineering. I am passionate about full-stack development, enjoy exploring new technologies, and pursue elegant engineering practices. I look forward to interacting and learning with fellow iGEMers so we can progress together!"/>
@@ -24,15 +24,15 @@
 
     <div class='team-name'>Algorithm Development</div>
     <div class='member-card'>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/xichenliao.webp'
                   name='Xichen Liao'
                   role='Algorithm Development'
                   bio="Hello everyone, I'm Xichen Liao. In our iGEM team, I'm primarily responsible for algorithm development and environment setup. My major is bioinformatics, and I enjoy playing basketball. I've greatly benefited from the iGEM experience and am truly grateful."/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/yingxu.webp'
                   name='Ying Xu'
                   role='Algorithm Development'
                   bio="Hi, I'm Ying Xu, a bioinformatics student passionate about French and music. In our iGEM team, I focus on developing algorithms. Looking forward to contributing to our project and its outcomes."/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/hengyuwang.webp'
                   name='Hengyu Wang'
                   role='Model Builder'
                   bio="Hello everyone, I'm Hengyu Wang. My major is mathematics, and I mainly take on the task of modeling in this competition. I am outgoing and enjoy playing badminton and table tennis. I hope to improve my ability to use mathematical theories to solve real-world problems in iGEM."/>
@@ -40,21 +40,21 @@
 
     <div class='team-name'>Software Development and Wiki Coding</div>
     <div class='member-card'>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/shuyiliu.webp'
                   name='Shuyi Liu'
                   role='Software Development / Wiki coding'
                   bio="Hi, I am Shuyi Liu, majoring in software engineering. I have an outgoing and lively personality, and I love exploring the beauty of life. I improved my front-end development skills in the iGEM competition and also met like-minded partners."/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/wenjianzhang.webp'
                   name='Wenjian Zhang'
                   role='Software Development'
                   bio="Hi, I'm Wenjian Zhang, a junior majoring in software engineering at Tongji University. I participated in the front-end development of Atlas.Y software. I learned and grew a lot working as a team in the competition and it was a wonderful and memorable experience to be part of the iGEM team!"/>
     </div>
     <div class='member-card' style='margin-top: 40px'>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/zhuobingzhao.webp'
                   name='Zhuobing Zhao'
                   role='Software Development'
                   bio="Hi, I'm Zhuobing Zhao, majoring in software engineering. I'm responsible for the front-end of the software. It's wonderful time that solving difficulties with my teammates. Thank all of you!"/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/jiaxinfan.webp'
                   name='Jiaxin Fan'
                   role='Software Development'
                   bio="Hello everyone! I am Jiaxin Fan, and my major is software engineering. I am part of the iGEM software team. I am quite introverted, but Tongji iGEM is a very interesting and warm team. I've learned a lot here and also made new friends."/>
@@ -62,15 +62,15 @@
 
     <div class='team-name'>Art Design</div>
     <div class='member-card'>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/yiranwan.webp'
                   name='Yiran Wan'
                   role='Head of Art Design'
                   bio="Hello, everyone! I am Yiran Wan, a student of media and communication design. In this project, I am the team leader of the design part, and I am involved in coordinating the design and production of this project's web application, wiki, and video scripts. I had a great time working with all the iGEM team members!"/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/xinyudang.webp'
                   name='Xinyu Dang'
                   role='Art Design'
                   bio="Hello everyone, I'm Xinyu Dang. I am currently studying media and communication design in Tongji University. I participated in the design of wiki and software. I have a deeper understanding of teamwork and made many friends through this competition. It's a memorable experience!"/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/zhiyewang.webp'
                   name='Zhiye Wang'
                   role='Art Design'
                   bio="Hi, I'm Zhiye Wang, a student majoring in media and communication design in Tongji University and I participated in the design of the wiki, software, IP and logo. In this competition, I met great teammates, and it was a great pleasure to work with everyone!"/>
@@ -78,11 +78,11 @@
 
     <div class='team-name'>Human Practices</div>
     <div class='member-card'>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/ziwenyao.webp'
                   name='Ziwen Yao'
                   role='Head of Entrepreneurship / Edu / HP'
                   bio="Hi, I'm Ziwen Yao, a student majoring in urban design. Initially, I joined the team due to my interest in public health and public policy. In this competition, I took on the responsibilities of entrepreneurship, education, and integrated human practice. I am truly grateful for this interdisciplinary collaboration opportunity."/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/xicui.webp'
                   name='Xi Cui'
                   role='Entrepreneurship / Edu / HP'
                   bio="Hello everyone, I'm Xi Cui, a student majoring in business administration, in this competition, I am responsible for education, entrepreneurship, and integrated human practice. I've grown a lot along the way,and I am very happy to work with you all to solve problems!"/>
@@ -93,15 +93,15 @@
       <div class='team-name' style='width: 698px'>PI</div>
     </div>
     <div class='member-card'>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/zhenhuabai.webp'
                   name='Zhenhua Bai'
                   role='Wet Lab'
                   bio="I'm Zhenhua Bai, a wet lab experimenter. I like blue, but hate orange. The most common words my elder martial sister says to me are: &quot;Don&apos;t forget to add orange loading&quot; and &quot;Don&apos;t add too much gel dye&quot;."/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/jingzhang.webp'
                   name='Dr. Jing Zhang'
                   role='Primary PI'
                   bio='She has provided crucial assistance in project management and advancement, funding support, and team building.'/>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/guohuichuai.webp'
                   name='Dr. Guohui Chuai'
                   role='Secondary PI'
                   bio='He helped us brainstorm the core of our project and gave us a lot of useful suggestions in model and data.'/>
@@ -109,7 +109,7 @@
 
     <div class='team-name'>Instructor</div>
     <div class='member-card'>
-      <MemberCard photo='design-materials/DrugProduction.webp'
+      <MemberCard photo='https://static.igem.wiki/teams/5503/test/yunzhekang.webp'
                   name='Dr. Yunzhe Kang'
                   role='Instructor'
                   bio='Retirement, here I come this year!'/>
