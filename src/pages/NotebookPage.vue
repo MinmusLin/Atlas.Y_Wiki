f3b201f9018da9ff9c4f832af422eb33a29c33d5
MinmusLin <minmuslin@outlook.com>
2024-10-01 10:37:17 +0800

    Update NotebookPage.vue

--- src/pages/NotebookPage.vue
+++ src/pages/NotebookPage.vue
@@ -49,391 +49,315 @@ const items = [
   },
   {
     title: '2 March 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
+    algorithm: [],
+    software: [],
+    design: [],
     hp: [
-      '123',
+      'Evaluated the potential social benefits of the project, including both production and academic aspects.'
     ]
   },
   {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
+    title: '9 March 2024',
+    algorithm: [],
+    software: [],
+    design: [],
     hp: [
-      '123',
+      'Distributed and collected 139 questionnaires from individuals with diverse professional backgrounds (mainly biology-related fields) to assess public awareness of subcellular localization and expectations for related software.'
     ]
   },
   {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
+    title: '16 March 2024',
+    algorithm: [],
+    software: [],
+    design: [],
     hp: [
-      '123',
+      'Planned and began a six-month public education initiative, designing various courses and games.',
+      'Conducted comprehensive surveys to extract three typical target user profiles, identifying pain points and expectations. Discovered demand timing among users.'
     ]
   },
   {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
+    title: '23 March 2024',
+    algorithm: [],
+    software: [],
+    design: [],
     hp: [
-      '123',
+      'Leveraged partnerships with Shanghai Juzhen Education Technology Co., Ltd. and Chengdu Baijiangjun Cultural Media Co., Ltd. to promote synthetic biology and subcellular localization concepts in Shanghai and rural areas.'
     ]
   },
   {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
+    title: '30 March 2024',
+    algorithm: [],
+    software: [],
+    design: [],
     hp: [
-      '123',
+      'Consulted with experts in gene editing system optimization and bioinformatics algorithm development, establishing the project framework and gathering algorithm suggestions. Confirmed the feasibility of the algorithms.',
+      'Formed a team with members from the School of Life Sciences, School of Software, College of Design and Innovation, and School of Economics and Management, with clear division of labor and each member contributing their expertise.'
     ]
   },
   {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
+    title: '6 April 2024',
+    algorithm: [],
+    software: [],
+    design: [],
     hp: [
-      '123',
+      'Continued consultations with experts in gene editing system optimization and bioinformatics algorithm development, solidifying the project framework and confirming algorithm feasibility.'
     ]
   },
   {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
+    title: '13 April 2024',
+    algorithm: [],
+    software: [],
+    design: [],
     hp: [
-      '123',
+      'Visited a molecular immunology and signal transduction lab to identify the specific needs for our software in the biology field.',
+      'Exchanged insights with past team members from our university to learn team management experiences.'
     ]
   },
   {
-    title: '24 February 2024',
+    title: '4 May 2024',
     algorithm: [
-      '123',
+      'Conducted an in-depth literature review to compare the performance and applicability of different algorithms. Developed the overall technical framework for the project.',
+      'Authored a technical report that provided a detailed comparison of the advantages and disadvantages of various algorithms, supporting the technical choices made for the project.'
     ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
+    software: [],
+    design: [],
     hp: [
-      '123',
+      'Held a seminar with Fudan University and ShanghaiTech University to discuss HP (Human Practices) and EDU (Education) strategies.'
     ]
   },
   {
-    title: '24 February 2024',
+    title: '18 May 2024',
     algorithm: [
-      '123',
+      'Selected models and algorithms that best fit the software\'s functional needs and clarified the project workflows. This included tasks such as building linker and signal peptide databases, predicting subcellular localization of fusion proteins, protein structure prediction algorithms, as well as protein stability and functionality scoring evaluation. Completed the theoretical framework for these algorithms.'
+    ],
+    software: [],
+    design: [],
+    hp: []
+  },
+  {
+    title: '1 June 2024',
+    algorithm: [
+      'Conducted an interview with Professor Wang Chenfei, incorporating feedback to refine the algorithm design.'
+    ],
+    software: [],
+    design: [],
+    hp: []
+  },
+  {
+    title: '15 June 2024',
+    algorithm: [
+      'Collected and organized linker and signal peptide sequence data, performing quality screening on the collected data.',
+      'Designed an efficient data table structure for building linker and signal peptide databases.'
+    ],
+    software: [],
+    design: [],
+    hp: []
+  },
+  {
+    title: '29 June 2024',
+    algorithm: [
+      'Assisted in making decisions for the front-end visualization of protein PDB files. Researched existing software and methods for displaying protein PDB files, including PyMOL, Chimera, VMD, etc., and eventually selected the web-based NGL Viewer.'
+    ],
+    software: [],
+    design: [],
+    hp: []
+  },
+  {
+    title: '6 July 2024',
+    algorithm: [
+      'Configured the testing server environment to ensure compatibility among models.',
+      'Following extensive learning and discussion within the team, commenced the deployment of various models, including DeepLoc, ESMfold, CAD score, Rosetta, and SCHEMA recombination programs.'
     ],
     software: [
-      '123',
+      '123'
+    ],
+    design: [],
+    hp: []
+  },
+  {
+    title: '13 July 2024',
+    algorithm: [
+      'Completed the deployment of DeepLoc and Rosetta while correcting and optimizing sections of code for CAD score, SCHEMA recombination program, and ESMfold.',
+      'Developed code to implement localized CAD scoring calculation and allowed users to choose custom options. Additionally, we created a set of automated testing scripts to swiftly verify the effectiveness of the models.'
+    ],
+    software: [
+      '123'
+    ],
+    design: [],
+    hp: []
+  },
+  {
+    title: '20 July 2024',
+    algorithm: [
+      'Fully deployed all models, compiled comprehensive technical documentation covering model functions, deployment processes, and user guides to facilitate efficient learning and operation by team members.'
+    ],
+    software: [
+      '123'
+    ],
+    design: [],
+    hp: []
+  },
+  {
+    title: '27 July 2024',
+    algorithm: [
+      'Conducted a comprehensive performance test for all deployed models, analyzing aspects such as accuracy, response time, and memory management.',
+      'Developed a next-phase model optimization plan, learning the specific steps to fine-tune evolutionary models.'
+    ],
+    software: [
+      '123'
     ],
     design: [
-      '123',
+      'Gained initial understanding of project design.',
+      'Interfaced with algorithm and software team members.'
     ],
     hp: [
-      '123',
+      'Collected average market prices of competing products.',
+      'Used the SWOT (Strengths, Weaknesses, Opportunities, Threats) model for competitive analysis and pricing positioning.'
     ]
   },
   {
-    title: '24 February 2024',
+    title: '3 August 2024',
     algorithm: [
-      '123',
+      'Collected experiment data required for fine-tuning evolutionary models, including .fasta sequence data, .pdb structure data, and DMS (Deep Mutational Scanning) data.',
+      'Wrote scripts for data cleaning and preprocessing. Began preliminary model optimization.'
     ],
     software: [
-      '123',
+      '123'
     ],
     design: [
-      '123',
+      'Conducted interface design for web application.'
+    ],
+    hp: []
+  },
+  {
+    title: '10 August 2024',
+    algorithm: [],
+    software: [
+      '123'
+    ],
+    design: [
+      'Conducted interface design for web application.'
     ],
     hp: [
-      '123',
+      'Held an exchange meeting with Northwestern Polytechnical University on August 9, discussing team management experiences.',
+      'Managed social media accounts on August 10, creating momentum and promoting the Atlas project.',
+      'Conducted an in-depth interview with DerYin Technology, extracting six key points and generating design measures.'
     ]
   },
   {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
+    title: '17 August 2024',
+    algorithm: [],
     software: [
-      '123',
+      '123'
     ],
     design: [
-      '123',
+      'Completed web application page design.'
     ],
     hp: [
-      '123',
+      'Designed a membership-based revenue model, algorithm store, and package-based revenue streams, and proposed advertising revenue strategies.'
     ]
   },
   {
-    title: '24 February 2024',
+    title: '24 August 2024',
     algorithm: [
-      '123',
+      'Designed a detailed computational experiment validation process, which included simulation steps, data collection, and analysis methods.',
+      'Conducted several small-scale simulations to ensure the feasibility and accuracy of the processes.'
     ],
     software: [
-      '123',
+      '123'
     ],
     design: [
-      '123',
+      'Completed the design and planning of the promotional video script.'
     ],
     hp: [
-      '123',
+      'Defined academic discount measures and assessed the social benefits of academic discounts.',
+      'Set personal and enterprise membership prices.',
+      'Set individual prices and package prices for the algorithm store.'
     ]
   },
   {
-    title: '24 February 2024',
+    title: '31 August 2024',
     algorithm: [
-      '123',
+      'Conducted computational experiment validation, using the DeepLoc model for large-scale protein localization predictions.',
+      'Visualized analysis of results to evaluate model performance in practical applications.'
     ],
     software: [
-      '123',
+      '123'
     ],
     design: [
-      '123',
+      'Completed the design and planning of the promotional video script.'
     ],
     hp: [
-      '123',
+      'Forecasted market trends and market size, adjusting pricing and revenue strategies accordingly.',
+      'Predicted revenue, estimated costs, analyzed profit and loss, calculated customer acquisition costs (CAC), and customer lifetime value (LTV) and its ratio. Envisioned future directions and created financial plans.'
     ]
   },
   {
-    title: '24 February 2024',
+    title: '7 September 2024',
     algorithm: [
-      '123',
+      'Developed and integrated scripts for algorithm automation, meeting the functional calls of all project models.',
+      'Conducted multiple tests in different environments to ensure script stability.',
+      'Completed interface testing with the software group to ensure seamless system integration.'
     ],
     software: [
-      '123',
+      '123'
     ],
     design: [
-      '123',
+      'Completed the production of project promotional video clips.',
+      'Conducted wiki design planning.'
     ],
     hp: [
-      '123',
+      'Developed a business canvas.',
+      'Used PESTEL (Political, Economic, Social, Technological, Environmental, Legal) analysis to assess risks in various areas and formulated risk mitigation measures.'
     ]
   },
   {
-    title: '24 February 2024',
+    title: '14 September 2024',
     algorithm: [
-      '123',
+      'Migrated optimized models to a high-performance computing server and performed large-scale processing tests. Monitored performance metrics such as memory usage and processing time.'
     ],
     software: [
-      '123',
+      '123'
     ],
     design: [
-      '123',
+      'Completed wiki home page design.',
+      'Completed the design of wiki unified templates for various boards.'
     ],
     hp: [
-      '123',
+      'Searched for accelerators and joined Tongji University’s Venture Valley.',
+      'Made reasonable stage predictions and expectations.'
     ]
   },
   {
-    title: '24 February 2024',
+    title: '21 September 2024',
     algorithm: [
-      '123',
+      'Fine-tuned server configurations to improve efficiency, focusing on enhancing computational speed and resource utilization',
+      'Conducted additional testing to validate the improvements mad'
     ],
     software: [
-      '123',
+      '123'
     ],
     design: [
-      '123',
+      'Completed the wiki animation design.'
     ],
     hp: [
-      '123',
+      'Developed four major marketing strategies.',
+      'Developed continuous revenue growth strategies'
     ]
   },
   {
-    title: '24 February 2024',
+    title: '28 September 2024',
     algorithm: [
-      '123',
+      'Assisted in authoring the project\'s Wiki documentation, focusing on the introduction of Model sections and the algorithm iteration process.'
     ],
     software: [
-      '123',
+      '123'
     ],
     design: [
-      '123',
+      'Improved the design and production of illustrations for various wiki pages.'
     ],
     hp: [
-      '123',
-    ]
-  },
-  {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
-    hp: [
-      '123',
-    ]
-  },
-  {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
-    hp: [
-      '123',
-    ]
-  },
-  {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
-    hp: [
-      '123',
-    ]
-  },
-  {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
-    hp: [
-      '123',
-    ]
-  },
-  {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
-    hp: [
-      '123',
-    ]
-  },
-  {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
-    hp: [
-      '123',
-    ]
-  },
-  {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
-    hp: [
-      '123',
-    ]
-  },  {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
-    hp: [
-      '123',
-    ]
-  },
-  {
-    title: '24 February 2024',
-    algorithm: [
-      '123',
-    ],
-    software: [
-      '123',
-    ],
-    design: [
-      '123',
-    ],
-    hp: [
-      '123',
+      'Envisioned the future of brand development'
     ]
   }
 ]
